a48f2c4d63620a3b3400c39f50be2a4181e0857d
nishithagentyala <nishithagentyala@gmail.com>
2025-04-08 12:56:21 +0530

    changes done

--- backend/usersRoute.js
+++ backend/usersRoute.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express.Router();
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const User = require("./userSchema");
 const jwt = require("jsonwebtoken");
 const session = require("express-session");
